052cbdbb34c3612d2f9d278862057169c9090981
Eduardocuevastzun <3031934760108@ingenieria.usac.edu.gt>
2024-10-29 19:00:53 -0600

    script migracion de datos

--- proyecto2/apigo.go
+++ proyecto2/apigo.go
@@ -1,10 +1,11 @@
-package proyecto2
+package main
 
 import (
 	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
+
 	"log"
 	"net/http"
 	"os"
@@ -19,20 +20,20 @@ import (
 )
 
 type artist struct {
-	Name      string
-	Birth     int
-	AreaBirth string
-	Biography string
+	Name      sql.NullString
+	Birth     sql.NullInt64
+	AreaBirth sql.NullString
+	Biography sql.NullString
 }
 
 type album struct {
-	Name     string
-	Language string
-	Version  string
+	Name     sql.NullString
+	Language sql.NullString
+	Version  sql.NullString
 }
 
 type label struct {
-	Name string
+	Name sql.NullString
 }
 
 // Función para cargar variables de entorno desde el archivo .env
@@ -77,7 +78,7 @@ func connectMongo() (*mongo.Client, *mongo.Collection, error) {
 	}
 
 	// Definir y retornar la colección que se utilizará
-	collection := client.Database("nombre_de_tu_base_de_datos").Collection("nombre_de_tu_coleccion")
+	collection := client.Database("proyectoFase2").Collection("artistas")
 	return client, collection, nil
 }
 
@@ -93,32 +94,41 @@ func migrarDatos(w http.ResponseWriter, r *http.Request, db *sql.DB, collection 
 
 	// Iterar sobre los resultados
 	for rows.Next() {
-		var nombre string
+		var nombre sql.NullString
+		var nombre2 string
 		if err := rows.Scan(&nombre); err != nil {
+			if err == sql.ErrNoRows {
+				log.Printf("Artista %s no encontrado", nombre)
+			}
 			log.Println("Error al escanear nombre:", err)
 			continue
 		}
 
+		if nombre.Valid {
+			nombre2 = nombre.String
+		} else {
+			continue
+		}
 		// Obtener información adicional
-		albumes, err := obtenerAlbumesPorArtista(nombre, w, db)
+		albumes, err := obtenerAlbumesPorArtista(nombre2, w, db)
 		if err != nil {
 			log.Printf("Error obteniendo álbumes para %s: %v", nombre, err)
 			continue
 		}
 
-		canciones, err := obtenerCancionesPorArtista(nombre, w, db)
+		canciones, err := obtenerCancionesPorArtista(nombre2, w, db)
 		if err != nil {
 			log.Printf("Error obteniendo canciones para %s: %v", nombre, err)
 			continue
 		}
 
-		disqueras, err := obtenerDisquerasPorArtista(nombre, w, db)
+		disqueras, err := obtenerDisquerasPorArtista(nombre2, w, db)
 		if err != nil {
 			log.Printf("Error obteniendo disqueras para %s: %v", nombre, err)
 			continue
 		}
 
-		artistaInfo, err := obtenerInformacionArtista(nombre, w, db)
+		artistaInfo, err := obtenerInformacionArtista(nombre2, w, db)
 		if err != nil {
 			log.Printf("Error obteniendo información del artista %s: %v", nombre, err)
 			continue
@@ -135,6 +145,7 @@ func migrarDatos(w http.ResponseWriter, r *http.Request, db *sql.DB, collection 
 			log.Printf("Error insertando en MongoDB para el artista %s: %v", nombre, err)
 			continue
 		}
+
 	}
 
 	// Enviar respuesta de éxito
@@ -147,18 +158,18 @@ func obtenerAlbumesPorArtista(nombre string, w http.ResponseWriter, db *sql.DB) 
 	// Definir la consulta SQL con el nombre como parámetro
 	query := `
 		SELECT DISTINCT ON (r.name)
-			r.name AS nombre_album, 
-			l.name AS lenguaje, 
-			r.comment AS version
-		FROM 
-			musicbrainz_old.release r
-			JOIN musicbrainz_old.artist a ON r.artist_credit = a.id
-			JOIN musicbrainz_old.language l ON r.language = l.id
-			JOIN musicbrainz_old.release_alias ra ON r.id = ra.release
-		WHERE 
-			a.name = $1
-		ORDER BY 
-			r.name, r.id;`
+            r.name AS nombre_album, 
+            l.name AS lenguaje, 
+            r.comment AS version
+        FROM 
+            musicbrainz_old.artist a
+            LEFT JOIN musicbrainz_old.release r ON r.artist_credit = a.id
+            LEFT JOIN musicbrainz_old.language l ON r.language = l.id
+            LEFT JOIN musicbrainz_old.release_alias ra ON r.id = ra.release
+        WHERE 
+            a.name = $1
+        ORDER BY 
+            r.name, r.id;`
 
 	// Ejecutar la consulta utilizando la variable 'nombre' como parámetro
 	rows, err := db.Query(query, nombre)
@@ -175,15 +186,18 @@ func obtenerAlbumesPorArtista(nombre string, w http.ResponseWriter, db *sql.DB) 
 	for rows.Next() {
 		var album1 album
 		if err := rows.Scan(&album1.Name, &album1.Language, &album1.Version); err != nil {
+			if err == sql.ErrNoRows {
+				log.Printf("album %s no encontrado", nombre)
+			}
 			http.Error(w, "Error al leer fila de PostgreSQL", http.StatusInternalServerError)
 			return nil, fmt.Errorf("error al leer fila de PostgreSQL: %v", err)
 		}
 
 		// Crear el objeto BSON y hacer append al slice albumes
 		album2 := bson.M{
-			"Nombre":   album1.Name,
-			"Lenguaje": album1.Language,
-			"Version":  album1.Version,
+			"Nombre":   album1.Name.String,
+			"Lenguaje": album1.Language.String,
+			"Version":  album1.Version.String,
 		}
 		albumes = append(albumes, album2)
 	}
@@ -219,22 +233,30 @@ func obtenerCancionesPorArtista(nombre string, w http.ResponseWriter, db *sql.DB
 	var canciones []bson.M
 
 	for rows.Next() {
-		var lengthMillis int
-		var trackName string
+		var lengthMillis sql.NullInt64
+		var trackName sql.NullString
+		var formattedDuration string
 		if err := rows.Scan(&trackName, &lengthMillis); err != nil {
+			if err == sql.ErrNoRows {
+				log.Printf("track %s no encontrado", nombre)
+			}
 			http.Error(w, "Error al leer fila de PostgreSQL", http.StatusInternalServerError)
 			return nil, fmt.Errorf("error al leer fila de PostgreSQL: %v", err)
 		}
 
-		// Convertir 'lengthMillis' a minutos y segundos
-		seconds := lengthMillis / 1000   // Convertir milisegundos a segundos
-		minutes := seconds / 60          // Calcular los minutos completos
-		remainingSeconds := seconds % 60 // Calcular los segundos restantes
-		formattedDuration := fmt.Sprintf("%02d:%02d", minutes, remainingSeconds)
+		if lengthMillis.Valid {
+			// Convertir 'lengthMillis' a minutos y segundos
+			seconds := lengthMillis.Int64 / 1000 // Convertir milisegundos a segundos
+			minutes := seconds / 60              // Calcular los minutos completos
+			remainingSeconds := seconds % 60     // Calcular los segundos restantes
+			formattedDuration = fmt.Sprintf("%02d:%02d", minutes, remainingSeconds)
+		} else {
+			formattedDuration = fmt.Sprintf("%02d:%02d", 0, 0)
+		}
 
 		// Crear el objeto BSON y hacer append al slice canciones
 		cancion := bson.M{
-			"Nombre":   trackName,
+			"Nombre":   trackName.String,
 			"Duracion": formattedDuration,
 		}
 		canciones = append(canciones, cancion)
@@ -248,15 +270,15 @@ func obtenerCancionesPorArtista(nombre string, w http.ResponseWriter, db *sql.DB
 func obtenerDisquerasPorArtista(nombre string, w http.ResponseWriter, db *sql.DB) ([]bson.M, error) {
 	// Definir la consulta SQL con el nombre como parámetro
 	query := `
-		SELECT DISTINCT
+		 SELECT DISTINCT
 			lb.name AS disquera
 		FROM 
-			musicbrainz_old.release r
-			JOIN musicbrainz_old.artist a ON r.artist_credit = a.id
-			JOIN musicbrainz_old.language l ON r.language = l.id
-			JOIN musicbrainz_old.release_alias ra ON r.id = ra.release
-			JOIN musicbrainz_old.release_label rl ON r.id = rl.release
-			JOIN musicbrainz_old.label_alias lb ON rl.label = lb.label
+			musicbrainz_old.artist a
+			LEFT JOIN musicbrainz_old.release r ON r.artist_credit = a.id
+			LEFT JOIN musicbrainz_old.language l ON r.language = l.id
+			LEFT JOIN musicbrainz_old.release_alias ra ON r.id = ra.release
+			LEFT JOIN musicbrainz_old.release_label rl ON r.id = rl.release
+			LEFT JOIN musicbrainz_old.label_alias lb ON rl.label = lb.label
 		WHERE 
 			a.name = $1;`
 
@@ -274,13 +296,16 @@ func obtenerDisquerasPorArtista(nombre string, w http.ResponseWriter, db *sql.DB
 	for rows.Next() {
 		var disquera label
 		if err := rows.Scan(&disquera.Name); err != nil {
+			if err == sql.ErrNoRows {
+				log.Printf("disquera %s no encontrado", nombre)
+			}
 			http.Error(w, "Error al leer fila de PostgreSQL", http.StatusInternalServerError)
 			return nil, fmt.Errorf("error al leer fila de PostgreSQL: %v", err)
 		}
 
 		// Crear el objeto BSON y hacer append al slice disqueras
 		disquera2 := bson.M{
-			"Nombre": disquera.Name,
+			"Nombre": disquera.Name.String,
 		}
 		disqueras = append(disqueras, disquera2)
 	}
@@ -292,7 +317,7 @@ func obtenerDisquerasPorArtista(nombre string, w http.ResponseWriter, db *sql.DB
 // Función para obtener información específica del artista por nombre
 func obtenerInformacionArtista(nombre string, w http.ResponseWriter, db *sql.DB) (bson.M, error) {
 	query := `
-		SELECT 
+		SELECT
 			a.name AS nombre,
 			a.begin_date_year AS anio_nacimimento,
 			ar.name AS lugar_nacimiento,
@@ -302,7 +327,7 @@ func obtenerInformacionArtista(nombre string, w http.ResponseWriter, db *sql.DB)
 			a.comment AS biografia
 		FROM
 			musicbrainz_old.artist a
-			JOIN musicbrainz_old.area ar ON a.area = ar.id
+			LEFT JOIN musicbrainz_old.area ar ON a.area = ar.id
 		WHERE 
 			a.name = $1;`
 
@@ -310,45 +335,68 @@ func obtenerInformacionArtista(nombre string, w http.ResponseWriter, db *sql.DB)
 	row := db.QueryRow(query, nombre)
 
 	var artista artist
-	var tipo, genero int
-	var estatus bool
+	var tipo, genero sql.NullInt64
+	var estatus sql.NullBool
+	var tipo2, genero2 int64
+	var tipo3, genero3, estatus3 string
+	var estatus2 bool
 
 	// Escanear los valores de la fila devuelta
 	if err := row.Scan(&artista.Name, &artista.Birth, &artista.AreaBirth, &tipo, &genero, &estatus, &artista.Biography); err != nil {
+		if err == sql.ErrNoRows {
+			log.Printf("Artista %s no encontrado", nombre)
+		}
 		return nil, fmt.Errorf("error al obtener información del artista: %v", err)
 	}
 
-	// Determinar los valores legibles para tipo, género y estatus
-	tipo2 := "desconocido"
-	if tipo == 1 {
-		tipo2 = "solista"
-	} else if tipo == 2 {
-		tipo2 = "grupo"
+	if tipo.Valid {
+		tipo2 = tipo.Int64
+		tipo3 = "desconocido"
+		if tipo2 == 1 {
+			tipo3 = "solista"
+		} else if tipo2 == 2 {
+			tipo3 = "grupo"
+		}
+	} else {
+		// Asignar un valor por defecto o manejar el caso nulo
+		tipo3 = "no aplica"
 	}
 
-	genero2 := "No aplica"
-	if genero == 1 {
-		genero2 = "Masculino"
-	} else if genero == 2 {
-		genero2 = "Femenino"
+	if genero.Valid {
+		genero2 = genero.Int64
+		genero3 = "No aplica"
+		if genero2 == 1 {
+			genero3 = "Masculino"
+		} else if genero2 == 2 {
+			genero3 = "Femenino"
+		}
+	} else {
+		// Asignar un valor por defecto o manejar el caso nulo
+		genero3 = "No aplica"
 	}
 
-	estatus2 := "No aplica"
-	if estatus == false {
-		estatus2 = "Activo"
-	} else if estatus == true {
-		estatus2 = "Retirado"
+	if estatus.Valid {
+		estatus2 = estatus.Bool
+		estatus3 = "No aplica"
+		if estatus2 == false {
+			estatus3 = "Activo"
+		} else if estatus2 == true {
+			estatus3 = "Retirado"
+		}
+	} else {
+		// Asignar un valor por defecto o manejar el caso nulo
+		estatus3 = "No aplica"
 	}
 
 	// Crear un objeto BSON para el documento del artista
 	artista1 := bson.M{
-		"Nombre":              artista.Name,
-		"Fecha de Nacimiento": artista.Birth,
-		"Pais":                artista.AreaBirth,
-		"Tipo":                tipo2,
-		"Genero":              genero2,
-		"Biografia":           artista.Biography,
-		"Estatus":             estatus2,
+		"Nombre":              artista.Name.String,
+		"Fecha de Nacimiento": artista.Birth.Int64,
+		"Pais":                artista.AreaBirth.String,
+		"Tipo":                tipo3,
+		"Genero":              genero3,
+		"Biografia":           artista.Biography.String,
+		"Estatus":             estatus3,
 	}
 
 	return artista1, nil
@@ -386,5 +434,8 @@ func main() {
 		port = "5001"
 	}
 	log.Printf("Servidor escuchando en el puerto %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	err = http.ListenAndServe(":"+port, r)
+	if err != nil {
+		return
+	}
 }
--- proyecto2/go.mod
+++ proyecto2/go.mod
@@ -1,4 +1,4 @@
-module proyectoFase2
+module proyecto2
 
 go 1.23.2
 
